908bcca2a558dada3984ab1797f62213a30cedf0
Adrian Gonzalez Garcia <adriangonzalezgarcia54@gmail.com>
2023-10-02 14:55:17 +0200

    Actividad 1 Acceso Datos modificado

--- src/Main.java
+++ src/Main.java
@@ -45,7 +45,13 @@ public class Main {
             //EJERCICIO 4
             System.out.println("\nLISTADO COMPLETO DE LOS DATOS DE LA TABLA STOCK\n");
 
-            resul = sentencia.executeQuery("SELECT cod_art,cod_f,cantidad FROM stock join fruteria on (stock.cod_f=fruteria.cod) join articulo on (articulo.cod=stock.cod_art)");
+            resul = sentencia.executeQuery("SELECT fruteria.nombre,articulo.nombre,articulo.categoria,stock.cantidad FROM stock join " +
+                    "fruteria on (stock.cod_f=fruteria.cod) join articulo on (articulo.cod=stock.cod_art)");
+
+            while(resul.next())
+            {
+                System.out.println(resul.getString(1) + " " + resul.getString(2) + " " + resul.getString(3)+ " " + resul.getString(4));
+            }
 
             resul.close();
             sentencia.close();
